31eb88f36d3f1046cf1eb27f984339134085128c
Jexxie <1989wjx@gmail.com>
2018-10-03 09:29:03 +0800

    iView布局 Affix 固定

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div style="width: 100%; height: 50px;position: fixed; left: 0;top: 0;background: red;z-index: 10;"></div>
+    <Row :gutter="32">
+      <Col span="16">
+        <Card>
+          <div style="height: 600px;">1111</div>
+        </Card>
+      </Col>
+      <Col span="8">
+        <Card>
+          <div style="height: 200px;">2222</div>
+        </Card>
+        <Affix :offset-top="50">
+          <Card>
+            <div style="height: 200px;">2222</div>
+          </Card>
+        </Affix>
+      </Col>
+    </Row>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    height: 5000px;
+    background: #808695 !important;
+  }
 </style>
+
